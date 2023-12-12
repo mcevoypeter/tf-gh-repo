d0a5432fd8337777aa670e5704caa83bd71ee368
Peter McEvoy <git@mcevoypeter.com>
2023-12-12 16:44:38 -0700

    Disable protected branches deployment policy
    The protected_branches and custom_branch_policies arguments of the
    deployment_branch_policy block cannot both be true.

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "github_repository_environment" "this" {
   environment = each.key
   repository  = github_repository.this.name
   deployment_branch_policy {
-    protected_branches     = true
+    protected_branches     = false
     custom_branch_policies = true
   }
 }
